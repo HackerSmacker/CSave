0e2542ad0fe91d57fc69535df7b398be7a1b251a
root@hpux11.hsnet.local <wec@mail.hackersmacker.net>
2020-10-21 09:00:53 -0500

    Windows build

--- FileTranslator.c
+++ FileTranslator.c
@@ -119,12 +119,26 @@ void readSave(uint8_t* save, FILE* file) {
 	save_t.engine_build = strtol(build, NULL, 10);
 END CRUSTY CODE */
 	fscanf(file, "%4c", save_t.header);
-	fscanf(file, "%" SCNd32, save_t.sg_version);
-	fscanf(file, "%" SCNd32, save_t.pkg_version);
+	fscanf(file, "%1" SCNd32, &save_t.sg_version);
+	fscanf(file, "%1" SCNd32, &save_t.pkg_version);
+	fscanf(file, "%1" SCNd16, &save_t.engine_major);
+	fscanf(file, "%1" SCNd16, &save_t.engine_minor);
+	fscanf(file, "%1" SCNd16, &save_t.engine_patch);
+	fscanf(file, "%1" SCNu32, &save_t.engine_build);
+	fscanf(file, "%1" SCNd32, &save_t.build_id_length);
+	save_t.build_id = malloc(save_t.build_id_length);
+	int i;
+	for(i = 0; i < save_t.build_id_length; i++) {
+		fscanf(file, "%c", save_t.build_id[i]);
+	}
+
 	printf("CSAV001RWS Save information:\n");
 	printf("Header: %s\n", save_t.header);
 	printf("SG version: %" SCNd32 "\n", save_t.sg_version);
-	printf("Package version: %" SCNd32 "\n", save_t.pkg_version);
+	printf("Engine version: %d.%d.%d\n", save_t.engine_major, save_t.engine_minor, save_t.engine_patch);
+	printf("Build ID: %s\n", save_t.build_id);
+
+
 	//printf("Engine version: %d.%d.%d\n", save_t.engine_major, save_t.engine_minor, save_t.engine_patch);
 	//printf("Engine build: %d\n", save_t.engine_build);
 }
--- FileTranslator.h
+++ FileTranslator.h
@@ -9,17 +9,18 @@
 size_t read_buffer(int max_length, uint8_t* out, FILE* f);
 
 struct Save {
-	char* header;		// 0-3
-	int32_t sg_version;		// 4-5
-	int32_t pkg_version;	// 6-7
-	int16_t engine_major;	// 8
-	int16_t engine_minor;	// 9
-	int16_t engine_patch;	// 10
-	uint32_t engine_build;	// 11-12
-	char* build_id;		// 13
-	int32_t fmt_version;
-	int32_t fmt_count;
-	char** custom_format_data;
+	char* header;		// 4
+	int32_t sg_version;	// 4
+	int32_t pkg_version;	// 4
+	int16_t engine_major;	// 2
+	int16_t engine_minor;	// 2
+	int16_t engine_patch;	// 2
+	uint32_t engine_build;	// 4
+	char* build_id;		// ?
+	int build_id_length;	// Not actually in the header
+	int32_t fmt_version;	// 4
+	int32_t fmt_count;	// 4
+	char** custom_format_data;	// ?
 	char* sg_type;
 };
 
