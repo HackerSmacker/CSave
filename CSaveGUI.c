494fe01195bdfebe1fb8aadb0836b3240b8e2919
root@hpux11.hsnet.local <wec@mail.hackersmacker.net>
2020-10-27 14:45:51 -0500

    Updated GUI and added a worthwhile Windows build

--- CSaveGUI.c
+++ CSaveGUI.c
@@ -27,6 +27,21 @@ int main(int argc, char** argv) {
 		else if(choice == '2') {
 			saveToProto();
 		}
+		else if(choice == '3') {
+			attron(COLOR_PAIR(4));
+			mvprintw(row - 1, 0, "Feature not implemented (yet).");
+			attroff(COLOR_PAIR(4));
+			getch();
+		}
+		else if(choice == '4') {
+			saveUnpack();
+		}
+		else if(choice == '5') {
+			attron(COLOR_PAIR(4));
+			mvprintw(row - 1, 0, "Feature not implemented (yet).");
+			attroff(COLOR_PAIR(4));
+			getch();
+		}
 		else {
 			attron(COLOR_PAIR(4));
 			mvprintw(row - 1, 0, "Please enter one of the options listed above.");
@@ -130,6 +145,61 @@ void saveToProto() {
 	if(fp == NULL) {
 		attron(COLOR_PAIR(4));
 		mvprintw(row - 1, 0, "Failed to execute SaveToProto!");
+		attroff(COLOR_PAIR(4));
+		refresh();
+		getch();
+		return;
+	}
+	int outRow = 4;
+	int outCol = 2;
+	attron(COLOR_PAIR(3));
+	move(outRow, outCol);
+	while(fgets(path, sizeof(path), fp) != NULL) {
+		char myLine[col - 4];
+		memcpy(myLine, path, col - 4);
+		if(outRow > row - 4) {
+			getch();
+			clear();
+			outRow = 4;
+		}
+		mvprintw(outRow, outCol, "%s", myLine);
+		outRow++;
+	}
+	pclose(fp);
+	getch();
+	attroff(COLOR_PAIR(3));
+	attron(COLOR_PAIR(2));
+	mvprintw(row - 1, 0, "Execution complete");
+	attroff(COLOR_PAIR(2));
+	getch();
+}
+
+void saveUnpack() {
+	clear();
+	attron(COLOR_PAIR(1));
+	mvprintw(0, (col - strlen("Save Info")) / 2, "Save Info");
+	attroff(COLOR_PAIR(1));
+	char filename[1024];
+	attron(COLOR_PAIR(2));
+	mvprintw(1, 0, "Please specify a filename.");
+	attroff(COLOR_PAIR(2));
+	attron(COLOR_PAIR(3));
+	refresh();
+	mvprintw((row - 2), 2, "Enter file --> ");
+	attroff(COLOR_PAIR(3));
+	getstr(filename);
+	attron(COLOR_PAIR(2));
+	mvprintw(2, 0, "Will process %s", filename);
+	refresh();
+	FILE* fp;
+	char path[8192];
+	char* command = malloc(strlen("SaveUnpack ") + strlen(filename) + 1);
+	strcpy(command, "SaveUnpack ");
+	strcat(command, filename);
+	fp = popen(command, "r");
+	if(fp == NULL) {
+		attron(COLOR_PAIR(4));
+		mvprintw(row - 1, 0, "Failed to execute SaveUnpack!");
 		attroff(COLOR_PAIR(4));
 		refresh();
 		getch();
