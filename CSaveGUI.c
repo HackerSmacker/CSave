c007d45a38a43a421f4114953c455ae1cfcdbdd9
root@hpux11.hsnet.local <wec@mail.hackersmacker.net>
2020-10-28 16:19:08 -0500

    Just about everything except item editing has been added.

--- CSaveGUI.c
+++ CSaveGUI.c
@@ -19,13 +19,15 @@ void drawMainMenu() {
 	attroff(A_BOLD);
 	attroff(COLOR_PAIR(1));
 	attron(COLOR_PAIR(2));
-	mvprintw(2, 2, "0    Settings");
-	mvprintw(3, 2, "1    Find Save Files");
+	mvprintw(2, 2, "0    File Save Files");
+	mvprintw(3, 2, "1    Find Protobuf Files");
 	mvprintw(4, 2, "2    Convert Save to Protobuf Format");
 	mvprintw(5, 2, "3    Convert Protobuf Format to Save");
 	mvprintw(6, 2, "4    Get Save Information");
-	mvprintw(7, 2, "5    Start Editor");
-	mvprintw(8, 2, "Z    Exit");
+	mvprintw(7, 2, "5    Get Profile Information");
+	mvprintw(8, 2, "6    Start Save Editor");
+	mvprintw(9, 2, "7    Start Profile Editor");
+	mvprintw(10, 2, "Z    Exit");
 	attroff(COLOR_PAIR(2));
 	attron(COLOR_PAIR(3));
 	mvprintw((row - 2), 2, "Enter option --> ");
@@ -40,7 +42,7 @@ void defineColors() {
 	init_pair(4, COLOR_RED, COLOR_BLACK);
 }
 
-void listDirectory(char* path) {
+void listDirectory(char* path, char* grep) {
 	struct dirent* de;
 	DIR* dr = opendir(path);
 	if(dr == NULL) {
@@ -62,8 +64,10 @@ void listDirectory(char* path) {
 	attron(COLOR_PAIR(2));
 	while((de = readdir(dr)) != NULL) {
 		move(line, 2);
-		printw("%s\n", de->d_name);
-		line++;
+		if(strstr(de->d_name, grep) != NULL) {
+			printw("%s\n", de->d_name);
+			line++;
+		}
 	}
 	attroff(COLOR_PAIR(2));
 	refresh();
@@ -235,6 +239,60 @@ void saveUnpack() {
 	getch();
 }
 
+void profUnpack() {
+	clear();
+	attron(COLOR_PAIR(1));
+	mvprintw(0, (col - strlen("Profile Info")) / 2, "Profile Info");
+	attroff(COLOR_PAIR(1));
+	char filename[1024];
+	attron(COLOR_PAIR(2));
+	mvprintw(1, 0, "Please specify a filename.");
+	attroff(COLOR_PAIR(2));
+	attron(COLOR_PAIR(3));
+	refresh();
+	mvprintw((row - 2), 2, "Enter file --> ");
+	attroff(COLOR_PAIR(3));
+	getstr(filename);
+	attron(COLOR_PAIR(2));
+	mvprintw(2, 0, "Will process %s", filename);
+	refresh();
+	FILE* fp;
+	char path[8192];
+	char* command = malloc(strlen("ProfileUnpack ") + strlen(filename) + 1);
+	strcpy(command, "ProfileUnpack ");
+	strcat(command, filename);
+	fp = popen(command, "r");
+	if(fp == NULL) {
+		attron(COLOR_PAIR(4));
+		mvprintw(row - 1, 0, "Failed to execute ProfileUnpack!");
+		attroff(COLOR_PAIR(4));
+		refresh();
+		getch();
+		return;
+	}
+	int outRow = 4;
+	int outCol = 2;
+	attron(COLOR_PAIR(3));
+	move(outRow, outCol);
+	while(fgets(path, sizeof(path), fp) != NULL) {
+		char myLine[col - 4];
+		memcpy(myLine, path, col - 4);
+		if(outRow > row - 4) {
+			getch();
+			clear();
+			outRow = 4;
+		}
+		mvprintw(outRow, outCol, "%s", myLine);
+		outRow++;
+	}
+	pclose(fp);
+	getch();
+	attroff(COLOR_PAIR(3));
+	attron(COLOR_PAIR(2));
+	mvprintw(row - 1, 0, "Execution complete");
+	attroff(COLOR_PAIR(2));
+	getch();
+}
 
 int main(int argc, char** argv) {
 	initscr();
@@ -248,8 +306,11 @@ int main(int argc, char** argv) {
 		if(choice == 'z') {
 			loopContinue = 0;
 		}
+		else if(choice == '0') {
+			listDirectory(".", ".sav");
+		}
 		else if(choice == '1') {
-			listDirectory(".");
+			listDirectory(".", ".proto");
 		}
 		else if(choice == '2') {
 			saveToProto();
@@ -261,10 +322,7 @@ int main(int argc, char** argv) {
 			saveUnpack();
 		}
 		else if(choice == '5') {
-			attron(COLOR_PAIR(4));
-			mvprintw(row - 1, 0, "Feature not implemented (yet).");
-			attroff(COLOR_PAIR(4));
-			getch();
+			profUnpack();
 		}
 		else {
 			attron(COLOR_PAIR(4));
